4540cd40bd2b05b2d9762957f200cc57f5f93e00
Rick Lee <rlee037@ucr.edu>
2019-11-01 15:29:14 -0700

    updated code

--- rlee037_lab7_part1.py
+++ rlee037_lab7_part1.py
@@ -1,29 +1,31 @@
 # lab 7 part 1
 
+import time
 import spidev
 
 def createSPI(device):
-  # Create the SPI object
-  # Open the SPI object on bus 0 for the specified device (CE)
-  # Set the max speed to 1 MHz
-  spi.max_speed_hz=1000000
-  # Set the mode to 0
-  spi.mode = 0
+  spi = spidev.SpiDev()    # Create the SPI object
+  spi.open(0, device)      # Open the SPI object on bus 0 for the specified device (CE)
+  spi.max_speed_hz=1000000 # Set the max speed to 1 MHz
+  spi.mode = 0             # Set the mode to 0
   return spi
 
 def WelfordsAlgorithm(val):
   # pp
 
 if __name__ == '__main__':
-  try:
-    while True:
-      # Receive data from microcontroller 1
-      # Calculate running average using Welford's algorithm
-      average = WelfordsAlgorithm(newLightValue)
-      # Send new value to microcontroller 2
-      # Delay for 1 second
-  except KeyboardInterrupt:
-      # Close all open SPI (spi.close())
-      exit()
+    try:
+        spi1 = createSPI(0)
+        spi2 = createSPI(1)
+        while True:
+            newLightValue = spi1.readbytes(1)          # Receive data from microcontroller 1
+            average = WelfordsAlgorithm(newLightValue) # Calculate running average using Welford's algorithm
+            spi2.xfer(average)                         # Send new value to microcontroller 2
+            time.sleep(1)                              # Delay for 1 second
+    except KeyboardInterrupt:
+        # Close all open SPI (spi.close())
+        spi1.close()
+        spi2.close()
+        exit()
 
 
